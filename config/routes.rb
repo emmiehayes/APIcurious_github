7e2dbbe7e56fae002f0ccb23515b7b1d851edb45
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-20 17:59:03 -0600

    add routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'sessions#new'
+  
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
+
+
+  # It accepts a parameter :provider in case you had multiple providers
+  # auth/failure is requested by the provider if the user fails to accept the requested permissions 
