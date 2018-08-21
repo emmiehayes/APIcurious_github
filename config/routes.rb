a93d73432b5ce75d647281ee5ebebd258fc00cca
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-20 18:26:36 -0600

    random spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root to: 'sessions#new'
-  
+
   get '/auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'logout', to: 'sessions#destroy', as: 'logout'
 end
-
-
-  # It accepts a parameter :provider in case you had multiple providers
-  # auth/failure is requested by the provider if the user fails to accept the requested permissions 
