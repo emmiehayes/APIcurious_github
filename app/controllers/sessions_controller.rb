6c0c2e02bec8c59511fd93d5244303e8f1880f1b
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-20 18:17:28 -0600

    add controller new action for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController 
 
+  def new 
+  end
+
   def create
     if user = User.from_omniauth(request.env["omniauth.auth"])
       session[:user_id] = user.id
