24df243bdcbf18a2567dc688d421d411758bacc3
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-21 15:26:54 -0600

    WIP building out feature testing

--- spec/features/user_can_see_basic_profile_info_spec.rb
+++ spec/features/user_can_see_basic_profile_info_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 feature 'a user visiting the user show page' do 
   scenario 'can see basic profile information' do 
     user = create(:user)
+    allow_any_instance_of(ApplicationController).to recieve(:current_user).and_return(user)
     visit '/dashboard'
 
-
     
-
+    expect(page).to have_content(user.username)
     expect(page).to have_css('.profile_pic')
     expect(page).to have_content('number of starred repos')
     expect(page).to have_content('number of followers')
