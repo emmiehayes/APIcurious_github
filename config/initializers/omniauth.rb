eeed54d519bb9308c53e8d2a117c46cad2c95a20
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-20 18:16:34 -0600

    update initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
 opts = { scope: 'user:email' }
+
+Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, Rails.application.secrets.github_client_id, Rails.application.secrets.github_client_secret, opts
 end
 
