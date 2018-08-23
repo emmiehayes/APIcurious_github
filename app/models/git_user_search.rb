bbd17f65c3b4a383c3fc88c3ad5a500aa9de9791
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-22 21:50:23 -0600

    pass username along with token through to service, map over repo list and pass to github user

--- app/models/git_user_search.rb
+++ app/models/git_user_search.rb
@@ -1,14 +1,20 @@
 class GitUserSearch 
 
-  def initialize(oauth_token)
-     @service = GithubService.new(oauth_token)
+  def initialize(oauth_token, username)
+     @service = GithubService.new(oauth_token, username)
   end
 
   def find_user 
-    github_user = GithubUser.new(data)
+    github_user = GithubUser.new(profile_data, repo_list)
   end
 
-  def data 
-    @service.raw_user
+  def profile_data 
+    @service.raw_user 
+  end
+
+  def repo_list
+    @service.raw_user_repos.map do |repo|
+      GithubRepo.new(repo)
+    end
   end
 end
